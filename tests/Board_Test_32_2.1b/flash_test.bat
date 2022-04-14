48c0d5bc0abb1e8bb7dcd7dffbf7f333c2a968ea
Kadah <Kadah.Coba@gmail.com>
2022-04-14 00:48:49 -0700

    Updated tests for newer esptool version

--- tests/Board_Test_32_2.1b/flash_test.bat
+++ tests/Board_Test_32_2.1b/flash_test.bat
@@ -1,5 +1,5 @@
 cd /D "%~dp0"
 
-%LocalAppData%\Arduino15\packages\esp32\tools\esptool_py\2.6.1/esptool.exe --chip esp32 --port COM27 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 %LocalAppData%\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/partitions/boot_app0.bin 0x1000 %LocalAppData%\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/bin/bootloader_dio_80m.bin 0x10000 ./Board_Test_32_2.1b.ino.bin 0x8000 ./Board_Test_32_2.1b.ino.partitions.bin
+%LocalAppData%\Arduino15\packages\esp32\tools\esptool_py\3.0.0\esptool.exe --chip esp32 --port COM3 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 %LocalAppData%\Arduino15\packages\esp32\hardware\esp32\1.0.5/tools/partitions/boot_app0.bin 0x1000 %LocalAppData%\Arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/bin/bootloader_dio_80m.bin 0x10000 ./Board_Test_32_2.1b.ino.bin 0x8000 ./Board_Test_32_2.1b.ino.partitions.bin
 
 pause
